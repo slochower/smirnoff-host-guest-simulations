4fd033ab09d15c9ebb62f13da00f49591692b1ae
David Slochower <slochower@gmail.com>
2018-05-24 18:30:23 -0700

    Overly high estimate of release free energy

---
+++ systems/a-bam-p/smirnoff/tscc-driver.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#PBS -l walltime=1:00:00,nodes=1:ppn=2 -q home-gibbs
+#PBS -j oe -r n
+#PBS -N SHG
+
+source /home/davids4/amber18_gnu.sh
+
+SCRDIR=/oasis/tscc/scratch/davids4/SHG-confirm/$PBS_JOBID
+mkdir -p $SCRDIR
+
+# Need the `-L` to resolve any links.
+rsync -avL $PBS_O_WORKDIR/ $SCRDIR/
+
+cd $SCRDIR
+source activate paprika
+python simulate.py
+
+rsync -avL $SCRDIR/ $PBS_O_WORKDIR/
