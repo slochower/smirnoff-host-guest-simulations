7c961100d1dff1b5337059a16eb0bb5443cabfea
David Slochower <slochower@gmail.com>
2018-10-19 15:11:22 -0700

    Housekeeping

---
+++ rsync-bg2bg2-tip3p.py
@@ -0,0 +1,53 @@
+import os
+import subprocess as sp
+from constants import systems
+
+for system in systems:
+    source = os.path.join("/gpfs/henrikse/bg2bg2-tip3p/", system, "fe")
+    destination = os.path.join("systems", system, "bg2bg2-tip3p")
+    if not os.path.isdir(destination):
+        print(f"Creating {destination}...")
+        os.makedirs(destination)
+
+    rsync_list = [
+        "rsync",
+        "-armv",
+        '-e "ssh"',
+        '--include="ti-a.dat"',
+        '--include="ti-u.dat"',
+        '--exclude="*"',
+        "{}".format("davids4@kirkwood:" + source + "/"),
+        "{}".format("."),
+    ]
+    p = sp.Popen([" ".join(rsync_list)], cwd=destination, shell=True)
+    output, error = p.communicate()
+    if p.returncode == 0:
+        pass
+    elif p.returncode == 1:
+        print("Output: {}".format(output))
+        print("Error: {}".format(error))
+
+for niel, dave in zip(["a-xxxx-s", "b-xxxx-s"], ["a-release", "b-release"]):
+    source = os.path.join("/gpfs/henrikse/bg2bg2-tip3p/", niel, "fe")
+    destination = os.path.join("systems", dave, "bg2bg2-tip3p")
+
+    if not os.path.isdir(destination):
+        print(f"Creating {destination}...")
+        os.makedirs(destination)
+
+    rsync_list = [
+        "rsync",
+        "-armv",
+        '-e "ssh"',
+        '--include="ti-r.dat"',
+        '--exclude="*"',
+        "{}".format("davids4@kirkwood:" + source + "/"),
+        "{}".format("."),
+    ]
+    p = sp.Popen([" ".join(rsync_list)], cwd=destination, shell=True)
+    output, error = p.communicate()
+    if p.returncode == 0:
+        pass
+    elif p.returncode == 1:
+        print("Output: {}".format(output))
+        print("Error: {}".format(error))
