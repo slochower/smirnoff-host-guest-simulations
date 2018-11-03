acda9a37c5aa8ded111be73a8f22bee2949e5fa0
David Slochower <slochower@gmail.com>
2018-11-03 11:02:17 -0700

    Snapshot

--- rsync-bg2bg2-tip3p.py
+++ rsync-bg2bg2-tip3p.py
@@ -27,6 +27,32 @@ for system in systems:
         print("Output: {}".format(output))
         print("Error: {}".format(error))
 
+for system in systems:
+    source = os.path.join("/gpfs/henrikse/bg2bg2-tip3p/", system, "a00")
+    destination = os.path.join("systems", system, "bg2bg2-tip3p")
+    if not os.path.isdir(destination):
+        print(f"Creating {destination}...")
+        os.makedirs(destination)
+
+    rsync_list = [
+        "rsync",
+        "-armv",
+        '-e "ssh"',
+        '--include="vac.topo"',
+        '--include="vac.crds"',
+        '--exclude="*"',
+        "{}".format("davids4@kirkwood:" + source + "/"),
+        "{}".format("."),
+    ]
+    p = sp.Popen([" ".join(rsync_list)], cwd=destination, shell=True)
+    output, error = p.communicate()
+    if p.returncode == 0:
+        pass
+    elif p.returncode == 1:
+        print("Output: {}".format(output))
+        print("Error: {}".format(error))
+
+
 for niel, dave in zip(["a-xxxx-s", "b-xxxx-s"], ["a-release", "b-release"]):
     source = os.path.join("/gpfs/henrikse/bg2bg2-tip3p/", niel, "fe")
     destination = os.path.join("systems", dave, "bg2bg2-tip3p")
